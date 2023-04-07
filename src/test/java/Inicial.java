23462d33ad4d5ff5daee111e044fde99246f1db3
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-04-07 17:20:41 -0300

    update

---
+++ src/test/java/Inicial.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Inicial {
+    @Test
+    public void verificar(){
+        Assertions.assertTrue(true);
+    }
+}
