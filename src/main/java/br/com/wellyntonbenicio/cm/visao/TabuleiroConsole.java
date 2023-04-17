0ada6aacf8edc97ef9b1d80583eb9e1a54c1889a
Wellynton Benicio <wellynton.benicio@gmail.com>
2023-04-17 19:59:04 -0300

    Criação Classe TabuleiroConsole

---
+++ src/main/java/br/com/wellyntonbenicio/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,12 @@
+package br.com.wellyntonbenicio.cm.visao;
+
+import br.com.wellyntonbenicio.cm.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+
+    private Tabuleiro tabuleiro;
+
+    public TabuleiroConsole(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+}
